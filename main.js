559638796e1344221e49de488bf0e0d5471b80c0
Vy <tuongvy.tran@gmail.com>
2022-11-12 21:31:34 -0600

    finalprojectfinal

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var form = document.getElementById('addForm');
-var itemList = document.getElementById('items')
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //Form submit event
 form.addEventListener('submit', addItem);
+// Delete Events
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e) {
@@ -27,7 +32,33 @@ function addItem(e) {
   deleteBtn.appendChild(document.createTextNode('X'));
   // Append button to li
   li.appendChild(deleteBtn);
-
   // Append li to item list
   itemList.appendChild(li);
+}
+
+// Remove Item
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure')) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e) {
+  //convert text to Lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
